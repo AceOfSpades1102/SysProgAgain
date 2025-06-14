e726111f0855beb49f36d9adfc96983589f2ccdd
Anna <schneiderannasophiek@gmail.com>
2025-06-14 22:13:01 +0200

    nur buchstabe

--- chat/src/user.h
+++ chat/src/user.h
@@ -24,7 +24,7 @@ User* searchUser(const char *name);
 
 extern User *userFront;
 
-//TODO: Add prototypes for functions that fulfill the following tasks:
+//TODONE: Add prototypes for functions that fulfill the following tasks:
 // * Add a new user to the list and start client thread
 // * Iterate over the complete list (to send messages to all users)
 // * Remove a user from the list
